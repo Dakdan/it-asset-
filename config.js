9a2f10d5c782264bf826ab7ba764bda5b8775311
Dakdan <khowit7@gmail.com>
2026-01-14 21:20:37 +0700

    Update config.js
    Signed-off-by: Dakdan <khowit7@gmail.com>

--- config.js
+++ config.js
@@ -1,18 +1,3 @@
-// ===== API =====
+// ================= CONFIG =================
 const GAS_API =
-  "https://script.google.com/macros/s/AKfycbwPg1Hwof0sUcM-gAvxBwE2PQ0HLdh_578aCHg_Cgq-JeQOcpQPm1zAE5C_uM7FqSg/exec";
-
-// ===== SESSION =====
-function setSession(user){
-  localStorage.setItem("user", JSON.stringify(user));
-}
-function getSession(){
-  return JSON.parse(localStorage.getItem("user") || "null");
-}
-function logout(){
-  localStorage.clear();
-  location.href = "index.html";
-}
-function requireLogin(){
-  if(!getSession()) location.href="index.html";
-}
+"https://script.google.com/macros/s/AKfycbwPg1Hwof0sUcM-gAvxBwE2PQ0HLdh_578aCHg_Cgq-JeQOcpQPm1zAE5C_uM7FqSg/exec";
