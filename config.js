b961548dbfb8b1a953d5217667ef95fe1b5a388d
Dakdan <khowit7@gmail.com>
2026-01-18 13:16:34 +0700

    Update config.js
    Signed-off-by: Dakdan <khowit7@gmail.com>

--- config.js
+++ config.js
@@ -1,27 +1,12 @@
-// ===== CONFIG =====
-const GAS_URL = "https://script.google.com/macros/s/AKfycbyaZCEVnz-5M-FC5Sr_8TJsrsnEk-gxwAW9iG0554doOqcwYOkP-pFueXGuZ4UO5MJDcg/exec";
+// config.js
+const GAS_URL = "https://script.google.com/macros/s/AKfycbwPg1Hwof0sUcM-gAvxBwE2PQ0HLdh_578aCHg_Cgq-JeQOcpQPm1zAE5C_uM7FqSg/exec";
 
-// ===== API CALL =====
-function api(action, params = {}) {
-  return new Promise((resolve, reject) => {
-    const cb = "cb_" + Date.now();
-    params.action = action;
-    params.callback = cb;
-
-    const query = Object.keys(params)
-      .map(k => `${encodeURIComponent(k)}=${encodeURIComponent(params[k])}`)
-      .join("&");
-
-    const s = document.createElement("script");
-    s.src = `${GAS_URL}?${query}`;
-
-    window[cb] = (res) => {
-      resolve(res);
-      delete window[cb];
-      document.body.removeChild(s);
-    };
-
-    s.onerror = reject;
-    document.body.appendChild(s);
-  });
+function api(action, data = {}) {
+  return fetch(GAS_URL, {
+    method: "POST",
+    body: JSON.stringify({ action, ...data }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(r => r.json());
 }
