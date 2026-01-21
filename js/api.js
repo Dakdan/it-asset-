bd2dbdd94c547685a3eb8dc75615bd5175c28bb8
Dakdan <khowit7@gmail.com>
2026-01-22 04:16:38 +0700

    Update api.js
    Signed-off-by: Dakdan <khowit7@gmail.com>

--- js/api.js
+++ js/api.js
@@ -1,18 +1,11 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbzNSgpYNigJX7W-RUPq8SLN4e687pE55p72KsbM-nWFcPefKDhjzYAflsm78i42IW7qrw/exec";
 
-async function apiGetJobs() {
-  const res = await fetch(API_URL);
-  return await res.json();
+async function fetchJobTypes() {
+  const res = await fetch(`${API_URL}?action=getJobTypes`);
+  return res.json();
 }
 
-async function apiUpdateJob(jobId, action, user) {
-  const res = await fetch(API_URL, {
-    method: "POST",
-    body: JSON.stringify({
-      jobId: jobId,
-      action: action,
-      user: user
-    })
-  });
-  return await res.json();
+async function fetchJobSubTypes(jobTypeCode) {
+  const res = await fetch(`${API_URL}?action=getJobSubTypes&jobTypeCode=${jobTypeCode}`);
+  return res.json();
 }
