f32a6d5733cffb33d880b79ad6e1257a2d56bb19
Dakdan <khowit7@gmail.com>
2026-01-22 04:26:34 +0700

    Update api.js
    Signed-off-by: Dakdan <khowit7@gmail.com>

--- js/api.js
+++ js/api.js
@@ -1,11 +1,16 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbzNSgpYNigJX7W-RUPq8SLN4e687pE55p72KsbM-nWFcPefKDhjzYAflsm78i42IW7qrw/exec";
+// 🔧 แก้ URL ให้เป็น Apps Script ของคุณ
+const API_BASE =
+  "https://script.google.com/macros/s/AKfycbzNSgpYNigJX7W-RUPq8SLN4e687pE55p72KsbM-nWFcPefKDhjzYAflsm78i42IW7qrw/exec";
 
-async function fetchJobTypes() {
-  const res = await fetch(`${API_URL}?action=getJobTypes`);
+async function apiGet(action) {
+  const res = await fetch(`${API_BASE}?action=${action}`);
   return res.json();
 }
 
-async function fetchJobSubTypes(jobTypeCode) {
-  const res = await fetch(`${API_URL}?action=getJobSubTypes&jobTypeCode=${jobTypeCode}`);
+async function apiPost(data) {
+  const res = await fetch(API_BASE, {
+    method: "POST",
+    body: JSON.stringify(data)
+  });
   return res.json();
 }
