8d3856fbdf8765532f3b0753f8ee9a801c00f85a
Dakdan <khowit7@gmail.com>
2026-01-22 04:33:37 +0700

    Update api.js
    Signed-off-by: Dakdan <khowit7@gmail.com>

--- js/api.js
+++ js/api.js
@@ -1,16 +1,7 @@
-// 🔧 แก้ URL ให้เป็น Apps Script ของคุณ
-const API_BASE =
-  "https://script.google.com/macros/s/AKfycbzNSgpYNigJX7W-RUPq8SLN4e687pE55p72KsbM-nWFcPefKDhjzYAflsm78i42IW7qrw/exec";
+const API =
+"https://script.google.com/macros/s/AKfycbzNSgpYNigJX7W-RUPq8SLN4e687pE55p72KsbM-nWFcPefKDhjzYAflsm78i42IW7qrw/exec";
 
-async function apiGet(action) {
-  const res = await fetch(`${API_BASE}?action=${action}`);
-  return res.json();
-}
-
-async function apiPost(data) {
-  const res = await fetch(API_BASE, {
-    method: "POST",
-    body: JSON.stringify(data)
-  });
-  return res.json();
+function apiGet(a){return fetch(API+"?action="+a).then(r=>r.json())}
+function apiPost(d){
+ return fetch(API,{method:"POST",body:JSON.stringify(d)}).then(r=>r.json())
 }
